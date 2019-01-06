0c20c1217c3f354bff4ed06638c419bca0b5a70c
hung <hungnguyen19897@gmail.com>
2019-01-06 17:43:48 +0700

    show

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-echo "capcone project 2 bluetooth low energy ";
 if(!empty($_GET))
-	print_r($_GET)
+	print_r($_GET);
+$myObj->research ="successful";
+$myJSON = json_encode($myObj);
+echo $myJSON;
 ?>
